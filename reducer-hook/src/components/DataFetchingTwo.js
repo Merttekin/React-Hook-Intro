9e6ce46c320e39d535a7cc34387e8215bebfddcd
MERT TEKIN <merttekin94@gmail.com>
2019-11-11 11:18:28 +0300

    done reducer-hook

---
+++ reducer-hook/src/components/DataFetchingTwo.js
@@ -0,0 +1,48 @@
+import React, { useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+const initialState = {
+    loading: true,
+    error: '',
+    post: {}
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS':
+            return {
+                loading: false,
+                post: action.payload,
+                error: ''
+            };
+        case 'FETCH_ERROR':
+            return {
+                loading: false,
+				post: {},
+				error: 'Something went wrong!'
+            };
+        default:
+            return state;
+    }
+}
+
+export default function DataFetchingTwo() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/1`)
+            .then(resp => {
+                dispatch({ type: 'FETCH_SUCCESS', payload: resp.data});
+            })
+            .catch(error => {
+                dispatch({ type: 'FETCH_ERROR'});
+            })
+    }, []);
+
+    return(
+        <div>
+            {state.loading ? 'Loading' : state.post.title}
+            {state.error ? state.error : null}
+        </div>
+    )
+}
